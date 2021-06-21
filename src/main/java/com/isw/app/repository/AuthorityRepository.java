aea9cb2840959a6667a65af45c9d2b5c4e4e3021
virlop <52721837+virlop@users.noreply.github.com>
2021-06-21 18:46:29 -0300

    Delete src directory

--- src/main/java/com/isw/app/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isw.app.repository;
-
-import com.isw.app.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
